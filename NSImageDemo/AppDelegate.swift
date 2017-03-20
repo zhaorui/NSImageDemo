40c2554f8139c1645f947f758ee0bc90f6e94124
zhaorui <zhaoruiexplorer@icloud.com>
2017-03-20 21:29:52 +0800

    Initial Commit

---
+++ NSImageDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSImageDemo
+//
+//  Created by 赵睿 on 20/03/2017.
+//  Copyright © 2017 com.homebrew.zhaorui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ NSImageDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSImageDemo
+//
+//  Created by 赵睿 on 20/03/2017.
+//  Copyright © 2017 com.homebrew.zhaorui. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
