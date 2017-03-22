fcca3f17482f0d6a442c2165585fba1d0d44dd1a
zhaorui <zhaoruiexplorer@icloud.com>
2017-03-22 10:57:58 +0800

    将左上角swift图片设置为圆形，方便观察。 技术要点： NSBezierPath.setClip() 限制绘制区域

--- NSImageDemo/ImageDemoView.swift
+++ NSImageDemo/ImageDemoView.swift
@@ -50,14 +50,31 @@ class ImageDemoView: NSView {
                       return true
                    })
     }()
+    
+    lazy var swift_medium_circle_image : NSImage = {
+        return NSImage(size: NSSize(width: image_square_edge, height: image_square_edge),
+                       flipped: false,
+                       drawingHandler: {rect in
+                        NSGraphicsContext.saveGraphicsState()
+                        let clip_path = NSBezierPath()
+                        clip_path.appendArc(withCenter: NSMakePoint(image_square_edge / 2.0, image_square_edge / 2.0),
+                                            radius: image_square_edge / 2.0 ,
+                                            startAngle: 0.0,
+                                            endAngle: 2.0*CGFloat(M_PI),
+                                            clockwise: true)
+                        clip_path.setClip()
+                        self.swift_medium_image.draw(at: NSZeroPoint, from: NSZeroRect, operation: .sourceOver, fraction: 1.0)
+                        NSGraphicsContext.restoreGraphicsState()
+                        return true
+                        })
+    }()
   
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         
-        //NSColor.green.set()
-        //NSBezierPath(rect: dirtyRect).fill()
-        
-        swift_medium_image.draw(at: top_left_image_origin, from: NSZeroRect, operation: top_left_operation, fraction: 1.0)
+        NSColor.green.set()
+        NSBezierPath(rect: dirtyRect).fill()
+        swift_medium_circle_image.draw(at: top_left_image_origin, from: NSZeroRect, operation: top_left_operation, fraction: 1.0)
         cpp_medium_image.draw(at: bottom_right_image_origin, from: NSZeroRect, operation: bottom_right_operation, fraction: 1.0)
     }
     
